b0e46711e34866993a98cea35229c7653cdb21df
SolMoonKnife <prd106401@naver.com>
2024-04-18 01:02:19 +0900

    algorithm 03 prob#04 main structure developed
    알고리즘 과제 03 prob#04 문제의 핵심 구조가 완성되었다. (Person 클래스 및 name 기반 비교 연산 정의, 이진 탐색 트리 연산, 파일 읽고 Person 객체로 이진 탐색 트리에 삽입, 중위 순회 출력 등)
    
    이슈: people 중위 순회 출력 시 예상치 못한 줄바꿈이 추가됨.

--- PyCharm/알고리즘_3차_과제/prob#04.py
+++ PyCharm/알고리즘_3차_과제/prob#04.py
@@ -8,10 +8,7 @@ def solution_04():
             self.phones = phones
             self.email = email
 
-        def get_name(self):
-            return self.name
-
-        def get_full_info(self):
+        def get_full_info(self):  # 현재 필요 없음
             return (
                 self.name,
                 self.address,
@@ -20,9 +17,23 @@ def solution_04():
                 self.phones,
                 self.email
             )
+
+        def get_str(self):
+            return (f"{self.name}\n"
+                    f"  Company: {self.company}\n"
+                    f"  Address: {self.address}\n"
+                    f"  Zipcode: {self.zipcode}\n"
+                    f"  Phones: {self.phones}\n"
+                    f"  Email: {self.email}" )
+
+        # 이진 탐색 트리의 key가 Person 객체이므로 key를 참조할 때 name을 확인할 수 있어야 한다
+        def __lt__(self, other):
+            return self.name < other.name
+        def __eq__(self, other):
+            return self.name == other.name
     class Node:
-        def __init__(self, name):
-            self.name = name
+        def __init__(self, key):
+            self.key = key
             self.left = None
             self.right = None
     class Binary_search_tree:
@@ -30,65 +41,89 @@ def solution_04():
             self.root = None
 
         def insert(self, key):
-            def _insert(root, name):
-                if root is None:
-                    return Node(name)
+            def _insert(node, key):
+                if not node:
+                    return Node(key)
 
-                if root.name < name:
-                    root.left = _insert(root.right, name)
-                elif root.name > name:
-                    root.right = _insert(root.left, name)
+                if key < node.key:
+                    node.left = _insert(node.left, key)
+                elif key > node.key:
+                    node.right = _insert(node.right, key)
 
-                return root
+                return node
 
             self.root = _insert(self.root, key)
 
-        def search(self, name):
-            def _search(root, name):
-                if root is not None:
-                    if root.name == name:
-                        return root
+            return self.root is not None  # 삽입 성공 여부
 
-                    if root.name < name:
-                        return _search(root.right, name)
-                    elif root.name > name:
-                        return _search(root.left, name)
+        def search(self, key):
+            def _search(node, key):
+                if not node:
+                    return None
+                # 키를 찾은 경우
+                if node.data == key:
+                    return node
+                # 오른쪽 또는 왼쪽을 탐색해야 할 경우
+                if node.data < key:
+                    return _search(node.right, key)
                 else:
+                    return _search(node.left, key)
+
+            return _search(self.root, key)
+
+        def delete(self, key):
+            def _get_successor(node):
+                min_key = node.key  # 최소 키 초기화
+                while node.left:    # 최대한 왼쪽 노드로 이동 (우측 서브 트리 내에서)
+                    min_key = node.left.key  # 왼쪽 노드가 존재하는 동안 왼쪽 키로 업데이트
+                    node = node.left  # 다음 왼쪽 노드로 이동
+
+                return min_key
+                
+            def _delete(node, key):
+                if not node:
                     return None
 
-            return _search(self.root, name)
-
-        def delete(self, name):
-            def _node_delete(node, parent):
-                if node == self.root:
-                    self.root = _delete(self.root)
-                elif node == parent.left:
-                    parent.left = _delete(node)
+                if key < node.key:          # 노드보다 키가 작으면 왼쪽으로 이동해서 지운다
+                    node.left = _delete(node.left, key)  # ★ 그리고 지운 결과 서브 트리를 왼쪽 노드에 연결한다.
+                elif key > node.key:
+                    node.right = _delete(node.right, key)
                 else:
-                    parent.right = _delete(node)
+                    if not node.left:       # 우선 한쪽 자식만 있는 경우를 처리
+                        return node.right   # 자식이 하나만 있으면 그 자식을 끌어 올리면 됨
+                    if not node.right:
+                        return node.left
 
-            def _delete(node):
-                if not node.left and not node.right:
-                    return None
-                elif not node.left and node.right:
-                    return node.right
-                elif node.left and not node.right:
-                    return node.left
-                else:
-                    begin = node.right
-                    parent = None  # 없어도 되는데 참조 오류 떠서 추가함
-                    while begin.left:
-                        parent = begin
-                        begin = begin.left
-                    self.root.data = begin.data
-                    if begin == node.right:
-                        self.root.right = begin.right
-                    else:
-                        parent.left = begin.right
-                    return self.root
-                    
+                    # 자식이 둘 있는 경우: 삭제 대상 노드의 키 값을 후계자 노드의 키 값으로 대체하고 후계자 노드를 삭제
+                    #
+                    node.key = _get_successor(node.right)  # 우측 서브 트리의 최소 키를 후계자 키로 선택 및 대체
+                    node.right = _delete(node.right, node.key)  # 대체한 키 값을 사용하여 우측 서브 트리의 후계자 노드 삭제
+            
+                return node  # 삭제(후계자 대체) 작업이 완료된 node 반환
+
+            self.root = _delete(self.root, key)
+            # 1) root is None: None
+            # 2) root has one chile: root.left OR root.right
+            # 3) root has two children: replace key with successor's key and delete successor node
+
+        def print_in_order(self):
+            def _print_in_order(root):
+                if root:
+                    _print_in_order(root.left)
+                    print(root.key.get_str())
+                    _print_in_order(root.right)
+
+            _print_in_order(self.root)
+
+    with open("address_book2020.tsv", "r") as file:
+        people_lines = file.readlines()
+
+    people = Binary_search_tree()
+    for line in people_lines[1:]:
+        name, address, company, zipcode, phones, email = tuple(line.split('\t'))
+        people.insert(Person(name, address, company, zipcode, phones, email))
+
+    people.print_in_order()
 
 
-
-    with open("address_book2020.tsv", "r") as file: pass
-
+solution_04()
