106a83086d9b1b01496a786ffe7298bfc8c65560
SolMoonKnife <prd106401@naver.com>
2024-04-19 11:15:12 +0900

    원신기원계산기 광추워프 추가
    붕괴: 스타레일의 광추 워프 규칙을 추가하였다. PICUP_TYPE을 "L"로 정하면 광추 픽업 규칙에 따라 80회 천장이 적용된다. 픽업 확률은 별도로 0.75로 지정해야 한다. 캐릭터는 "C"로 지정하고, 원신과 스타레일에 동일하게 적용된다.

--- PyCharm/개인_프로젝트/원신기원계산기.py
+++ PyCharm/개인_프로젝트/원신기원계산기.py
@@ -4,30 +4,52 @@
 import numpy as np
 import math as mt
 import time as tm
-def gacha(n):
+def gacha(n, pickupRate=0.5, type="C"):
     rate = lambda x: 1.0 if x == 90 else 0.006 + (0 if x < 74 else 0.06 * (x - 73))
+    rate_core = lambda x: 1.0 if x == 80 else 0.006 + (0 if x < 64 else 0.06 * (x - 63))
     stack = 0
     mustup = 0
     win = [0, 0]
     pickdown, pickup = 0, 0
-    while n > 0:
-        stack += 1
-        key = np.random.rand()
-        if rate(stack) >= key:
-            if mustup == 1:
-                mustup = 0
-                pickup += 1
-            else:
-                picksel = np.random.rand()
-                if 0.5 >= picksel:
+    if type == "C":
+        while n > 0:
+            stack += 1
+            key = np.random.rand()
+            if rate(stack) >= key:
+                if mustup == 1:
+                    mustup = 0
                     pickup += 1
-                    win[0] += 1
                 else:
-                    mustup = 1
-                    pickdown += 1
-                    win[1] += 1
-            stack = 0
-        n -= 1
+                    picksel = np.random.rand()
+                    if pickupRate >= picksel:
+                        pickup += 1
+                        win[0] += 1
+                    else:
+                        mustup = 1
+                        pickdown += 1
+                        win[1] += 1
+                stack = 0
+            n -= 1
+    elif type == "L":
+        while n > 0:
+            stack += 1
+            key = np.random.rand()
+            if rate_core(stack) >= key:
+                if mustup == 1:
+                    mustup = 0
+                    pickup += 1
+                else:
+                    picksel = np.random.rand()
+                    if pickupRate >= picksel:
+                        pickup += 1
+                        win[0] += 1
+                    else:
+                        mustup = 1
+                        pickdown += 1
+                        win[1] += 1
+                stack = 0
+            n -= 1
+
     return pickup, pickdown, win
 
 def gacha_idle(n, x, r):
@@ -39,22 +61,21 @@ def gacha_idle(n, x, r):
     return sum(nCk(n, k) * (r ** k) * ((1-r) ** (n-k)) for k in range(x, n+1))
 
 print(
-    "가챠 스택에 따른 확률은 다음 공식에 따라 연산됩니다.",
-    "\nrate = 1.0 if x == 90 else 0.006 + (0 if x < 74 else 0.06 * (x - 73))",
-    "\n90스택일 경우 100% / 1 ~ 73스택일 경우 0.6% / 74 ~ 89스택일 경우 0.6% + 73초과 스택 당 6%",
-    "\n매번 독립 시행 시 key값을 무작위로 설정합니다. 범위: 0.0 ~ 1.0",
-    "\nkey값이 스택에 따른 확률보다 작은 값인 경우 ★★★★★ 등장으로 간주합니다.",
-    "\n★★★★★ 등장 시 확정 천장일 경우 픽업 아이템이 등장합니다.",
-    "\n★★★★★ 등장 시 확정 천장이 아닐 경우 픽업 key값을 무작위로 설정합니다. 범위: 0.0 ~ 1.0",
-    "\n픽업 key값이 0.5이하인 경우 픽업 아이템이 등장하고, 0.5초과인 경우 픽뚫 아이템이 등장합니다.",
-    "\n픽뚫 아이템이 등장한 경우 다음 ★★★★★ 등장 시 확정 천장으로 설정됩니다.\n"
+    "TRY = 시행 횟수\n" +
+    "PICKUP_RATE = 픽업 확률\n" +
+    "PICKUP_TYPE = 픽업 유형(캐릭터 \"C\" / 광추 \"L\")\n"
 )
 
-TRY = 1000000000
+TARGET = "반디 전광"
+TRY = 70
+PICKUP_RATE = 0.75
+PICKUP_TYPE = "L"
+
 start = tm.time()
-x, y, win = gacha(TRY)
+x, y, win = gacha(TRY, PICKUP_RATE, PICKUP_TYPE)
 end = tm.time()
+
 print(f"시행 {TRY}회, 연산 시간 {(end - start):.{4}f}초")
-print(f"유라 {x}개, 픽뚫 {y}개")
-print(f"유라 획득 확률: {100*x/TRY}%")
+print(f"{TARGET} {x}개, 픽뚫 {y}개 (픽업 확률 {(PICKUP_RATE*100):.{1}f}%)")
+print(f"{TARGET} 획득 확률: {100*x/TRY}%")
 print(f"★★★★★ 획득 확률: {100*(x+y)/TRY}%")
